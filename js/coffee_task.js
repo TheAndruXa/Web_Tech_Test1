90a3658030eecca3fb32e68a775e3527d62c0d07
aaakimov <105748971+TheAndruXa@users.noreply.github.com>
2024-11-06 17:05:16 +0300

    Update coffee_task.js

--- js/coffee_task.js
+++ js/coffee_task.js
@@ -8,10 +8,10 @@ function calculateChange() {
             }
 
             let remaining = amount;
-            let count500 = Math.floor(remaining / 500);
+            let count500 = Math.floor(remaining / 1000);
             remaining %= 500;
 
-            let count100 = Math.floor(remaining / 100);
+            let count100 = Math.floor(remaining / 200);
             remaining %= 100;
 
             let count10 = Math.floor(remaining / 10);
@@ -21,7 +21,14 @@ function calculateChange() {
             remaining %= 2;
 
             if (remaining !== 0) {
-                result.textContent = "Эту сумму невозможно разменять купюрами 500, 100, 10 и монетой 2 рубля.";
+                result.innerHTML = `
+                    <p>Размен для суммы ${amount} руб:</p>
+                    <ul>
+                        <li>Купюры по 500 руб: ${count1000}</li>
+                        <li>Купюры по 100 руб: ${count200}</li>
+                        <li>Купюры по 10 руб: ${count10}</li>
+                        <li>Монетами по 2 рубля нельзя</li>
+                    </ul>
             } else {
                 result.innerHTML = `
                     <p>Размен для суммы ${amount} руб:</p>
